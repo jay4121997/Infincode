f44ddc0bbfd57d71684673aa0e5345b4cfea3a90
Jay Trivedi <36271236+jay4121997@users.noreply.github.com>
2024-11-11 23:24:26 -0500

    Analytics enables

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,26 +1,45 @@
-import Head from "next/head"
-import Footer from "./ui/Footer"
-import Navbar from "./ui/Navbar"
+import Head from "next/head";
+import Footer from "./ui/Footer";
+import Navbar from "./ui/Navbar";
+import { Analytics } from "@vercel/analytics/react";
 
 const Layout = ({ children }) => {
-    return (
-        <>
-            <Head>
-                <title>Infinicode</title>
-                <meta name='description' content='Infinicode making 
-                it simple for you to build and manage your Data pipelins, Integrate AI and ML in your business, Get useful business insights from your Data and much more. Reach out to us today at contact.insightfulmind@gmail.com' />
-                <meta name='viewport' content='width=device-width, initial-scale=1' />
-                <link rel='icon' href='/favicon.ico' />
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-                <link rel="manifest" href="/site.webmanifest"></link>
-            </Head>
-            <Navbar />
-            <main>{children}</main>
-            <Footer />
-        </>
-    )
-}
+  return (
+    <>
+      <Analytics />
+      <Head>
+        <title>Infinicode</title>
+        <meta
+          name="description"
+          content="Infinicode making 
+                it simple for you to build and manage your Data pipelins, Integrate AI and ML in your business, Get useful business insights from your Data and much more. Reach out to us today at contact.insightfulmind@gmail.com"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest"></link>
+      </Head>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
